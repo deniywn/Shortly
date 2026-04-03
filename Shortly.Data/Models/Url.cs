be96ccef41003e83ddcfcd7a9bd32b871fb701ed
deniywn19 <deniywn19@gmail.com>
2026-04-03 08:21:35 +0700

    43E_CreateApplicationModels

---
+++ Shortly.Data/Models/Url.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data.Models
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public string OriginalLink { get; set; }
+        public string ShortLink { get; set; }
+        public int NrOfClicks { get; set; }
+        public int? UserId { get; set; }
+
+
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+    }
+}
---
+++ Shortly.Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            Urls = new List<Url>();
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public List<Url> Urls { get; set; }
+    }
+}
