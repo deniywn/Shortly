b39c9baf03b5a15a22b5819ae4c3958629e1a3b6
deniywn19 <deniywn19@gmail.com>
2026-04-03 14:24:43 +0700

    53E_LoadUsersFromDBwithEF

--- Shortly.Client/Controllers/AuthenticationController.cs
+++ Shortly.Client/Controllers/AuthenticationController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shortly.Client.Data.ViewModels;
+using Shortly.Data;
 
 namespace Shortly.Client.Controllers
 {
     public class AuthenticationController : Controller
     {
+        private AppDbContext _context;
+        public AuthenticationController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Users()
         {
-            return View();
+            var users = _context.Users.ToList();
+            return View(users);
         }
 
         public IActionResult Login()
@@ -17,7 +25,7 @@ namespace Shortly.Client.Controllers
 
         public IActionResult LoginSubmitted(LoginVM loginVM)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View("Login", loginVM);
             }
@@ -32,7 +40,7 @@ namespace Shortly.Client.Controllers
 
         public IActionResult RegisterUser(RegisterVM registerVM)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View("Register", registerVM);
             }
