0c2192129df1c0f6da732099d534a72a0f8b883c
deniywn19 <deniywn19@gmail.com>
2026-04-03 16:02:36 +0700

    58E_DefineIUrlsServiceInterface

--- Shortly.Data/Services/UrlsService.cs
+++ Shortly.Data/Services/UrlsService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Shortly.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Shortly.Data.Services
 {
-    public class UrlsService
+    public class UrlsService : IUrlsService
     {
         private AppDbContext _context;
         public UrlsService(AppDbContext context)
@@ -14,5 +16,30 @@ namespace Shortly.Data.Services
             _context = context;
         }
 
+        public Url GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Url> GetUrls()
+        {
+            var allUrls = _context.Urls.Include(n => n.User).ToList();
+            return allUrls;
+        }
+
+        public Url Add(Url url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Url Update(int id, Url url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Shortly.Data/Services/UsersService.cs
+++ Shortly.Data/Services/UsersService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Shortly.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +8,38 @@ using System.Threading.Tasks;
 
 namespace Shortly.Data.Services
 {
-    public class UsersService
+    public class UsersService : IUsersService
     {
         private AppDbContext _context;
         public UsersService(AppDbContext context)
         {
             _context = context;
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUsers()
+        {
+            var users = _context.Users.Include(n => n.Urls).ToList();
+            return users;
+        }
+
+        public User Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(int id, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
